e4536d5f475086e83c85f52e747bb1ce06b49df0
NiCoLas5342 <43699406+NiCoLas5342@users.noreply.github.com>
2020-09-22 14:59:38 +0200

    Personnes

---
+++ Personnes/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+// REMPLACE LA CLASSE TESTPERSONNE
+namespace Personnes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            personne nicolas = new personne("Chanourdie", "Nicolas", 22);
+            personne hugo = new personne("Test", "hugo", 19);
+            Console.WriteLine("Personnes : "+personne.Combien());
+        }
+    }
+}
---
+++ Personnes/personne.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace Personnes
+{
+    class personne
+    {
+        private string nom;
+        private string prenom;
+        private int age;
+
+        public personne(string nom, string prenom, int age)
+        {
+            this.nom = nom;
+            this.prenom = prenom;
+            this.age = age;
+            count ++;
+        }
+        private static int count
+        {
+            get;
+            set;
+        }
+
+        public string GetNom()
+        {
+            return nom;
+        }
+
+        public string GetPrenom()
+        {
+            return prenom;
+        }
+
+        public int GetAge()
+        {
+            return age;
+        }
+        public void afficheUnePersonne()
+        {
+            Console.WriteLine("Nom : " + GetNom() +", Prenom : " + GetPrenom() + ", Age : " + GetAge());
+        }
+
+        public static int Combien()
+        {
+            return count;
+        }
+    }
+
+}
