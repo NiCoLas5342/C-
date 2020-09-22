9348b8f21954323daa3b3b8375998c5579355aca
NiCoLas5342 <43699406+NiCoLas5342@users.noreply.github.com>
2020-09-22 11:55:56 +0200

    EXERCICE ARTICLE

---
+++ LesBases/Article/article.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Article
+{
+    class article
+    {
+        protected string nom;
+        protected int prix;
+        protected int quantite;
+        
+        public string GetNom()
+        {
+            return nom;
+        }
+
+        public int GetPrix()
+        {
+            return prix;
+        }
+
+        public int GetQuantite()
+        {
+            return quantite;
+        }
+
+        protected void SetPrix(int value)
+        {
+            this.prix = value; 
+        }
+
+        protected void SetQuantite(int value)
+        {
+            this.quantite = value;
+        }
+
+        public void Afficher() 
+        {
+            Console.WriteLine(" Nom :"+ this.GetNom() +", Prix : "+ this.GetPrix() +", Quantite : "+ this.GetQuantite());
+        }
+        public void Ajouter(int number)
+        {
+            this.SetQuantite(this.GetQuantite + number);
+        }
+
+        public void Retirer( int number)
+        {
+            this.SetQuantite(this.GetQuantite - number);
+        }
+    }
+}
---
+++ LesBases/Exos-CNET/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Exos_CNET
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
