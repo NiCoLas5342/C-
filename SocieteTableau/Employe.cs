44529e291cb5fedb89b9e878fd9138f7711d0f71
NiCoLas5342 <43699406+NiCoLas5342@users.noreply.github.com>
2020-09-24 09:55:33 +0200

    SocieteTableau

---
+++ SocieteTableau/Employe.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SocieteTableau
+{
+    class Employe : Personne
+    {
+        private decimal salaire;
+        public Employe (string nom, string prenom, int age, decimal salaire) : base(nom, prenom, age)
+        {
+            this.salaire = salaire ;
+        }
+
+        public int GetSalaire()
+        {
+            return salaire;
+        }
+
+        public override String ToString()
+        {
+            return ("Salaire :" + this.GetSalaire());
+        }
+
+        public virtual void afficher()
+        {
+            Console.WriteLine(this.ToString());
+        }
+    }
+}
---
+++ SocieteTableau/Personne.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace SocieteTableau
+{
+    class Personne
+    {
+        private string nom;
+        private string prenom;
+        private int age;
+
+        public Personne(string nom, string prenom, int age)
+        {
+            this.nom = nom;
+            this.prenom = prenom;
+            this.age = age;
+        }
+
+        public string GetNom()
+        {
+            return nom;
+        }
+
+        public string GetPrenom()
+        {
+            return prenom;
+        }
+
+        public int GetAge()
+        {
+            return age;
+        }
+        public Personne()
+        {
+
+        }
+        public override String ToString()
+        {
+            return ("Nom :" + this.GetNom());
+        }
+
+        public virtual void afficher()
+        {
+            Console.WriteLine(this.ToString());
+        }
+
+        public static Personne operator ++(Personne pers)
+        {
+            pers.age++;
+        }
+    }
+}
