db3bf22461c4a61be642d75056c8c5b096264823
NiCoLas5342 <43699406+NiCoLas5342@users.noreply.github.com>
2020-09-30 13:50:10 +0200

    Add files via upload

---
+++ EXO3_6/Article.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EXO3
+{
+    class Article
+    {
+        private string name;
+        private string description;
+        private int price;
+
+        public Article(string name, string description, int price)
+        {
+            this.name = name;
+            this.description = description;
+            this.price = price;
+        }
+
+        public string Name { get => name; set => name = value; }
+        public string Description { get => description; set => description = value; }
+        public int Price { get => price; set => price = value; }
+        public int Quantity { get => quantity; set => quantity = value; }
+
+        override
+        public string ToString()
+        {
+            return $"Name : {Name}, Description : {Description}, Price : {Price}";
+        }
+    }
+}
