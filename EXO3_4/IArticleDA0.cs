6cf923c73adcdb889cf0c5c95981070b9e290f27
NiCoLas5342 <43699406+NiCoLas5342@users.noreply.github.com>
2020-09-30 13:43:17 +0200

    Add files via upload

---
+++ EXO3_4/IArticleDA0.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EXO3
+{
+    class IArticleDA0
+    {
+        interface IArticleDAO
+        {
+            public Article GetArticleByName(string name);
+            public List<Article> GetArticlesByPrice(int price);
+            public List<Article> GetArticles();
+        }
+    }
+}
