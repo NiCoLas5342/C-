2ef634aa9d996029332b4b3567a14fb1db325eee
NiCoLas5342 <43699406+NiCoLas5342@users.noreply.github.com>
2020-09-30 13:54:13 +0200

    Update Article.cs

--- EXO3_5/Article.cs
+++ EXO3_5/Article.cs
@@ -8,23 +8,26 @@ namespace EXO3
     {
         private string name;
         private string description;
+        private int quantity;
         private int price;
 
-        public Article(string name, string description, int price)
+        public Article(string name, string description, int quantity, int price)
         {
             this.name = name;
             this.description = description;
+            this.quantity = quantity;
             this.price = price;
         }
 
         public string Name { get => name; set => name = value; }
         public string Description { get => description; set => description = value; }
         public int Price { get => price; set => price = value; }
+        public int Quantity { get => quantity; set => quantity = value; }
 
         override
         public string ToString()
         {
-            return $"Name : {Name}, Description : {Description}, Price : {Price}";
+            return $"Name : {Name}, Description : {Description}, Price : {Price}, Qantity : {quantity}";
         }
     }
 }
