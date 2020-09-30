c055efcf6f21306c74cac9c1b5dfd161fe43da0a
NiCoLas5342 <43699406+NiCoLas5342@users.noreply.github.com>
2020-09-30 13:52:44 +0200

    Add files via upload

---
+++ EXO3_7/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace EXO3
+{
+    class Program
+    {
+         static void Main(string[] args)
+        {
+            List<Article> articlesList = new List<Article>()
+            {
+                new Article("My first program","Hello World",15,1),
+                new Article("What is web development","JS,HTML,CSS",9,4),
+                new Article("Who created React","Facebook",10,2),
+            };
+
+            ArticleDAO dao = new ArticleDAO(articlesList);
+            Console.WriteLine(dao.GetArticlesInformations());
+        
+        }
+    }
+}
+
